f8523688c21d4746615d6dce9083a8d6cf5d605b
BrantK <brant@cyberdust.com>
2015-11-10 18:55:19 -0800

    Added junit output to Run button, but it executes after completion

--- CDAutomation/src/application/AutomationApp.java
+++ CDAutomation/src/application/AutomationApp.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-
 import javax.swing.DefaultListModel;
 import javax.swing.DropMode;
 import javax.swing.JButton;
@@ -13,6 +12,7 @@ import javax.swing.JList;
 import javax.swing.border.BevelBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+
 import java.awt.Color;
 
 public class AutomationApp {
@@ -41,33 +41,31 @@ public class AutomationApp {
 	private void initialize() {
 		myFrame = new JFrame();
 		myFrame.setTitle("CD Automation");
-		myFrame.setBounds(100, 100, 460, 400);
+		myFrame.setBounds(300, 300, 460, 400);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.getContentPane().setLayout(null);
 
 		// List of test files
 		DefaultListModel<String> simpleList = new FileFinder().simpleFileList();
 		JList<String> listOfTests = new JList<String>(simpleList);
-		JScrollPane scrollPane = new JScrollPane();
-		
-		scrollPane.setBounds(10, 11, 137, 181);
-		scrollPane.setViewportView(listOfTests);
-		
+		JScrollPane testListScroll = new JScrollPane();
+		testListScroll.setViewportView(listOfTests);
+		testListScroll.setBounds(10, 10, 137, 181);
 		listOfTests.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		myFrame.getContentPane().add(scrollPane);
+		myFrame.getContentPane().add(testListScroll);
 		
 		// Drag and drop rearrange - TODO
 		listOfTests.setDropMode(DropMode.INSERT);
 		listOfTests.setDragEnabled(true);
 
-		// Console window
+		// Console output window
 		JTextPane consoleOut = new JTextPane();
 		consoleOut.setBackground(Color.LIGHT_GRAY);
 		consoleOut.setEditable(false);
 		consoleOut.setBounds(10, 203, 424, 118);
 		consoleOut.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		myFrame.getContentPane().add(consoleOut);
-		
+
 		// Open log button
 		JButton logButton = new JButton("Open Log");
 		logButton.addActionListener(new ActionListener() {
@@ -83,13 +81,26 @@ public class AutomationApp {
 		logButton.setBounds(70, 328, 90, 25);
 		myFrame.getContentPane().add(logButton);
 		
+		// junit output window - TODO Change from jlist 
+		DefaultListModel<String> methodList = new DefaultListModel<String>();
+		JList<String> junitOut = new JList<String>();
+		junitOut.setBackground(Color.WHITE);
+		junitOut.setFocusable(false);
+		JScrollPane junitScroll = new JScrollPane();
+		junitScroll.setViewportView(junitOut);
+		junitScroll.setBounds(250, 10, 180, 180);
+		junitOut.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		myFrame.getContentPane().add(junitScroll);
+		
 		// Run button
 		JButton runButton = new JButton("Run");
 		runButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				List<String> selectedTests = listOfTests.getSelectedValuesList();
+				methodList.removeAllElements();
+
 				try {
-					TestFinder.allTests(selectedTests);
+					TestExecuter.allTests(selectedTests);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
