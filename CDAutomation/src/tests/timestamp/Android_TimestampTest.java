70bb5458f9f1778ee0cee7f2449c4ce0b39e2ecc
Manjushri <manjushri1611@gmail.com>
2016-01-29 11:16:23 -0800

    Working on timestamp

--- CDAutomation/src/tests/timestamp/Android_TimestampTest.java
+++ CDAutomation/src/tests/timestamp/Android_TimestampTest.java
@@ -13,6 +13,25 @@ import io.appium.java_client.TouchAction;
 public class Android_TimestampTest extends Drivers {
 	
 	public void test01_timestamp_test() throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		String account_name = "timestamptest";
+		String account_pw = "password";
+		String account_friend = "timestamptestfriend";
 		
 	}
+	String text_message = "CyberDust.com";
+	TouchAction action = new TouchAction(driver);
+	LoginWith loginAs = new LoginWith();
+	
+
+	int sw = driver.manage().window().getSize().getWidth();
+	int sh = driver.manage().window().getSize().getHeight();
+	
+	public static void relaunch() {
+		driver.closeApp();
+		driver.launchApp();
+	}
+	
+	
+	
 }
