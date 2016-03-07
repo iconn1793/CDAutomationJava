ddc45fe184cb57791f0af0515d6b6cc51df0cdc8
BrantK <brant@cyberdust.com>
2016-03-07 14:20:59 -0800

    Updated to work with Android Studio

--- CDAutomation/src/application/FileFinder.java
+++ CDAutomation/src/application/FileFinder.java
@@ -11,7 +11,7 @@ public class FileFinder {
 	
 	// For custom test list order, add tests here from top to bottom
 	public List<String> testOrder () {
-		List<String> myOrder = new ArrayList<String>();
+		List<String> myOrder = new ArrayList<>();
 		
 		//////////////////////////////
 		myOrder.add("run_signup");
@@ -23,14 +23,20 @@ public class FileFinder {
 	
 	// Gets the absolute file path
 	public DefaultListModel<String> testFilePath (String dirName, DefaultListModel<String> files) {
-		DefaultListModel<String> myList = new DefaultListModel<String>();
+		DefaultListModel<String> myList = new DefaultListModel<>();
 	    File dir = new File(dirName);
 
 	    // Get all files from directory and sub directories
 	    File[] fList = dir.listFiles();
 	    for (File file : fList) {
-	        if (file.isFile() && file.getAbsolutePath().contains("Run")) {
-	            files.addElement(file.getAbsolutePath().replace("bin", "").replace("src", "").replace(".java", "").replace(".class", ""));
+
+	        if (file.isFile() && file.getAbsolutePath().toLowerCase().contains("run_")
+                    && file.getAbsolutePath().toLowerCase().contains("src")
+                    && !file.getAbsolutePath().toLowerCase().contains("out/production/")
+                    && !file.getAbsolutePath().toLowerCase().contains("out\\production\\")) {
+
+	            files.addElement(file.getAbsolutePath().replace(".java", ""));
+
 	        } else if (file.isDirectory()) {
 	            testFilePath(file.getAbsolutePath(), files);
 	        }
@@ -42,14 +48,15 @@ public class FileFinder {
 	    }
 	    
 	    // Puts the specified tests from testOrder() at the top of the list
-	    List<String> sortedList = new ArrayList<String>();
+	    List<String> sortedList = new ArrayList<>();
 	    for (int i = 0; i < myList.size(); i++) {
 	    	sortedList.add(myList.get(i));
 	    }
-	    
+
 	    myList.removeAllElements();
+
 	    for (String list : sortedList) {
-	    	
+
 	    	for (int i = 0; i < testOrder().size(); i++) {
 	    		if (list.toLowerCase().contains(testOrder().get(i))) {
 		    		myList.add(0, list);
@@ -60,17 +67,17 @@ public class FileFinder {
 	    		myList.addElement(list);
 	    	}
 	    }
-	    
+
 		return myList;
 	}
 	
 	// Removes the path and extension
 	public DefaultListModel<String> simpleFileList() {
 		String myDir = Paths.get("").toAbsolutePath().normalize().toString();
-		DefaultListModel<String> fileList = new DefaultListModel<String>();
+		DefaultListModel<String> fileList = new DefaultListModel<>();
 		DefaultListModel<String> rawList = new FileFinder().testFilePath(myDir, fileList);
-		DefaultListModel<String> simpleList = new DefaultListModel<String>();
-		
+		DefaultListModel<String> simpleList = new DefaultListModel<>();
+
 		for (int i = 0; i < rawList.size(); i++) {
 			 simpleList.addElement(rawList.get(i).substring(rawList.get(i).indexOf("Run"), rawList.get(i).length()).replace("Run_", "").replace("Run", ""));
 		}
